0d723e0f8f5705992626d0ec4d9728f62c50e0fb
Favour Felix <favour@acumen.digital>
2022-10-31 00:56:02 +0100

    feat: added functionality, missing image finesse

---
+++ app.js
@@ -0,0 +1,49 @@
+const cameraVideoStream = document.getElementById('camera-stream')
+const shutterButton = document.getElementById('shutter')
+const photosButton = document.getElementById('photos-btn')
+const gallery = document.querySelector('.gallery-view')
+const currentImageElement = document.querySelector('.gallery-view img')
+const closeGalleryButton = document.getElementById('close-gallery')
+const nextButton = document.getElementById('next')
+const prevButton = document.getElementById('prev')
+
+let height = innerHeight
+let width = innerWidth
+let streaming = false
+
+cameraVideoStream.height = height
+cameraVideoStream.width = width
+
+const capturedImages = []
+const currentImage = 0
+
+// Connect media device
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia()) {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then ((stream) => {
+    cameraVideoStream.srcObject = stream
+    cameraVideoStream.play()
+  })
+}
+
+// Capture snapshots using HTML Canvas
+function captureImage () {
+  const canvas = document.createElement('canvas')
+  canvas.height = height
+  canvas.width = width
+  document.body.appendChild(canvas)
+  const canvasContext = canvas.getContext('2d')
+  canvasContext.drawImage(cameraVideoStream, 5, 0, height, width)
+
+  // Convert captured data to image (base64)
+  const data = canvas.toDataURL('image/png')
+  currentImageElement.src = data
+  photosButton.style.backgroundImage = `url(${data})`
+  capturedImages.push(data)
+}
+shutterButton.addEventListener('click', () => captureImage())
+
+// Event handlers to close and open gallery
+photosButton.addEventListener('click', () => gallery.classList.add('show-gallery'))
+closeGalleryButton.addEventListener('click', () => gallery.classList.remove('show-gallery'))
+
+// Event handlers to browse gallery (next and previous)
